24ebd6e7506828288af23d450c4149ea2dfee640
sachin328071 <156741906+sachin328071@users.noreply.github.com>
2025-03-05 11:00:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsmember(){         //function to display the skills of the member
+    var x = document.getElementById("skills");
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+    
+
+}
